798380f53c017bc09f6eaf7fcb68ded9ac4ea92f
Sora <99982155+SoraSwmpai@users.noreply.github.com>
2025-04-16 01:07:24 +0530

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem; //read doc zora
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private InputAction thrust; //named it thurst it takes input action to thurst
+    [SerializeField] private InputAction thrust;
+    [SerializeField] private InputAction Rotation;
+    [SerializeField] private float thrustStrength;//named it thurst it takes input action to thurst
 
     private Rigidbody rb;
 
@@ -16,19 +18,25 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        Rotation.Enable();
     }
-
-    private void Update()
-    {
-        if (thrust.IsPressed())
-        {
-            
-        }
-    }
-
 
     private void FixedUpdate()
     {
-        throw new NotImplementedException();
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = Rotation.ReadValue<float>();
     }
 }
