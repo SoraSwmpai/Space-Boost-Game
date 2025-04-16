a6e560cb8a0c6a58a400af43e871886b35b96d5f
Sora <99982155+SoraSwmpai@users.noreply.github.com>
2025-04-16 18:18:58 +0530

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -6,13 +6,16 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private InputAction thrust;
     [SerializeField] private InputAction Rotation;
-    [SerializeField] private float thrustStrength;//named it thurst it takes input action to thurst
+    [SerializeField] private float rotationStrength = 100f;
+    [SerializeField] private float thrustStrength = 100f;//named it thurst it takes input action to thurst
 
     private Rigidbody rb;
+    private AudioSource audioSource;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnEnable()
@@ -32,11 +35,35 @@ public class Movement : MonoBehaviour
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+
+        else
+        {
+            audioSource.Stop();
         }
     }
 
     private void ProcessRotation()
     {
         float rotationInput = Rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }
